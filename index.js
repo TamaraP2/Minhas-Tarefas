428cfa1cc6ab476797cc562293085a9d27813b09
TamaraP2 <tamara-peixoto@hotmail.com>
2022-10-27 14:35:50 -0300

    HTML Ok

--- index.js
+++ index.js
@@ -3,3 +3,20 @@
 /* ====================== VARIAVEIS ===================== */
 /* ====================================================== */
  
+let tarefas = [];
+ 
+
+document.querySelector(".button").addEventListener("click", function(e) {
+    e.preventDefault();
+    console.log(tarefas);
+    
+    if (document.getElementById("tarefa").value !== "" && document.getElementById("duracao").value !== "") { 
+
+        let tarefa = {nome: document.getElementById("tarefa").value, duracao: Number(document.getElementById("duracao").value)};
+
+        tarefas.push(tarefa);
+
+        document.querySelector(".tarefas").insertAdjacentHTML("afterbegin", `<p class="lista-tarefas" id="tarefa-${tarefas.length}"></p>`);
+        document.getElementById(`tarefa-${tarefas.length}`).innerText = document.getElementById("tarefa").value;
+    }
+})
