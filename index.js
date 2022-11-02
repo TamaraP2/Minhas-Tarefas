e45b1c3e6c03732bca5ad34de8ba38c2da8efb95
TamaraP2 <tamara-peixoto@hotmail.com>
2022-11-02 17:24:23 -0300

    Perde duracao depois do terceiro drop

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ document.querySelector(".button").addEventListener("click", function(e) {
         
         calculaHorarios(); 
 
-        let tarefa = {index: tarefas.length, nome: document.getElementById("tarefa").value, duracao: document.getElementById("duracao").value, inicio: inicioTarefa, final: finalTarefa};
+        let tarefa = {posicao: tarefas.length, nome: document.getElementById("tarefa").value, duracao: document.getElementById("duracao").value, inicio: inicioTarefa, final: finalTarefa};
         
         tarefas.push(tarefa);     
         
@@ -37,7 +37,7 @@ document.querySelector(".button").addEventListener("click", function(e) {
         document.getElementById("duracao").value = "";
         document.getElementById("tarefa").focus(); 
 
-        document.querySelector(".console").insertAdjacentHTML("beforeend", `tarefas: ${JSON.stringify(tarefas[tarefas.length-1])} <br>`); 
+        // document.querySelector(".console").insertAdjacentHTML("beforeend", `tarefas: ${JSON.stringify(tarefas[tarefas.length-1])} <br>`); 
 
         dragAndDrop();  
 
@@ -143,17 +143,15 @@ function dragAndDrop() {
 
     
     document.querySelector(".tarefas").addEventListener("drop", function (event) { 
-
+        
         if (bool === false) {
-
+   
             let posicaoInicialItemMovimentado = -1;
             let posicaoFinalItemMovimentado; 
 
             for (let i = 0; i < document.querySelectorAll(".tarefa-item").length; i++) {
 
-                let idNumber = document.querySelectorAll(".tarefa-item")[i].id.slice(-1);
-
-                document.querySelector(".console").insertAdjacentHTML("beforeend", `i = ${i}; idNumber = ${idNumber} <br>`); 
+                let idNumber = document.querySelectorAll(".tarefa-item")[i].id.slice(-1); 
 
                 if (idNumber !== i && posicaoInicialItemMovimentado === -1) {
                     posicaoInicialItemMovimentado = i;  
@@ -165,99 +163,126 @@ function dragAndDrop() {
             }
  
             bool = true;
+  
+            let itemMovimentado = tarefas.splice(posicaoInicialItemMovimentado, 1);  
 
-            document.querySelector(".console").insertAdjacentHTML("beforeend", `posicaoInicialItemMovimentado = ${posicaoInicialItemMovimentado} <br>`); 
-            document.querySelector(".console").insertAdjacentHTML("beforeend", `posicaoFinalItemMovimentado = ${posicaoFinalItemMovimentado} <br>`); 
- 
+            tarefas.splice(posicaoFinalItemMovimentado, 0, itemMovimentado[0]);
+
+            // document.querySelector(".console").insertAdjacentHTML("beforeend", `TAREFAS DEPOIS = ${JSON.stringify(tarefas)} <br>`);  
+            // console.log("tarefas após splice: " + JSON.stringify(tarefas));
+            // FALTA RECALCULAR HORARIOS e ATUALIZAR IDS
+            
+            recalculaHorarios ();
         }
     });
 
 }
 
 
-function novasPosicoesTarefas() {
-  
-    let listaTarefas = [...document.querySelectorAll(".tarefa-item")];
 
-    document.querySelector(".console").insertAdjacentHTML("beforeend", `ENTROU novasPosicoesTarefas() <br>`); 
-    document.querySelector(".console").insertAdjacentHTML("beforeend", `tarefas: ${JSON.stringify(tarefas)} <br>`); 
+function recalculaHorarios () {
+
     
-    novasTarefas = [...tarefas];
-    
-    document.querySelector(".console").insertAdjacentHTML("beforeend", `novas tarefas: ${JSON.stringify(novasTarefas)} <br>`); 
+    for (let i = 0; i < tarefas.length; i++) {
 
-    novasTarefas.forEach((tarefa, index) => {
+        i === 0 ? tarefas[i].inicio = horarioInicial : tarefas[i].inicio = tarefas[i-1].final 
 
-        document.querySelector(".console").insertAdjacentHTML("beforeend", `ENTROU NO FOREACH DE NOVAS TAREFAS <br>`); 
+        tarefas[i].posicao = i; 
         
-        document.querySelector(".console").insertAdjacentHTML("beforeend", `ANTES - novas tarefas[${index}]: ${JSON.stringify(novasTarefas[index])} <br>`); 
-
-        calculaHorarios2(index);
-        tarefa.inicio = inicioTarefa; 
-        tarefa.final = finalTarefa;
-        document.querySelectorAll(".tarefa-item")[index].id = `tarefa-${index+1}`;
-        let textoTarefa = `${tarefa.inicio} - ${tarefa.nome} (${duracaoString})`; 
-        document.getElementById(`tarefa-${index+1}`).innerText = textoTarefa; 
-
-        document.querySelector(".console").insertAdjacentHTML("beforeend", `DEPOIS - novas tarefas[${index}]: ${JSON.stringify(novasTarefas[index])} <br>`); 
-    }); 
-    
-    // for (let i = 0; i < document.querySelectorAll(".tarefa-item").length; i++) {
-    //     document.querySelectorAll(".tarefa-item").id = `tarefa-${i}`;
-    // }
+        calculaHorarios2 (i);
+        
+        tarefas[i].final = finalTarefa;
  
+    }
+
+    document.querySelectorAll(".tarefa-item").forEach((cadaTarefa, index) => {
+
+        cadaTarefa.id = `tarefa-${index}`;
+          
+        let horarioInicioTarefa = tarefas[index].inicio;  
+        let texto = document.getElementById(`tarefa-${index}`).innerText.slice(5); 
+        let novoTexto = horarioInicioTarefa + texto; 
+
+        document.getElementById(`tarefa-${index}`).innerText = novoTexto;
+    })
+
+    document.querySelector(".console").insertAdjacentHTML("beforeend", `TAREFAS FINAL = ${JSON.stringify(tarefas)} <br>`);  
 }
 
+  
 
+function calculaHorarios2 (posicao) {  
+  
+    // novasTarefas.length === 0 ? inicioTarefa = horarioInicial : inicioTarefa = novasTarefas[novasTarefas.length-1].final;
 
-
-function calculaHorarios2 (index) {  
-
-    novasTarefas.length === 0 ? inicioTarefa = horarioInicial : inicioTarefa = novasTarefas[novasTarefas.length-1].final;
-
-    let inicioTarefaTotal = inicioTarefa.split(":");
+    let inicioTarefaTotal = tarefas[posicao].inicio.split(":");  
     let inicioTarefaHoras = Number(inicioTarefaTotal[0]);
-    let inicioTarefaMinutos = Number(inicioTarefaTotal[1]);
+    let inicioTarefaMinutos = Number(inicioTarefaTotal[1]); 
 
-    let duracaoTotal = novasTarefas[index].duracao;
+    let duracaoTotal = tarefas[posicao].duracao.split(":");  
     let duracaoHoras = Number(duracaoTotal[0]);
-    let duracaoMinutos = Number(duracaoTotal[1]);
-        
-    formatacaoDuracao2(duracaoHoras, duracaoMinutos);
-
+    let duracaoMinutos = Number(duracaoTotal[1]); 
+         
     let horaExtra = 0;
 
-    let somaMinutos = inicioTarefaMinutos + duracaoMinutos;
+    let somaMinutos = inicioTarefaMinutos + duracaoMinutos;  
 
     if (somaMinutos > 59) {        
         somaMinutos = somaMinutos - 60;
         horaExtra++; 
     }
 
-    let somaHoras = inicioTarefaHoras + duracaoHoras + horaExtra;
+    let somaHoras = inicioTarefaHoras + duracaoHoras + horaExtra; 
     
     if (somaHoras > 23) {
         somaHoras = somaHoras - 24;
     }
 
-    let finalTarefaParcial = [somaHoras.toString().padStart(2, "0"), ":", somaMinutos.toString().padStart(2, "0")];
+    let finalTarefaParcial = [somaHoras.toString().padStart(2, "0"), ":", somaMinutos.toString().padStart(2, "0")];  
 
-    finalTarefa = finalTarefaParcial.join(""); 
- 
+    finalTarefa = finalTarefaParcial.join("");   
+  
 }
 
-
-function formatacaoDuracao2(duracaoHoras, duracaoMinutos) {
  
-    if (duracaoHoras === 0) {
-        duracaoString = duracaoMinutos.toString() + "min";
-    }
-    else {
-        if (duracaoMinutos === 0) {
-            duracaoString = duracaoHoras.toString() + "h";  
-        }
-        else {
-            duracaoString = duracaoHoras.toString() + "h" + duracaoMinutos.toString() + "min";  
-        }
-    } 
-}
+
+
+
+
+
+
+
+
+
+// function novasPosicoesTarefas() {
+  
+//     let listaTarefas = [...document.querySelectorAll(".tarefa-item")];
+
+//     document.querySelector(".console").insertAdjacentHTML("beforeend", `ENTROU novasPosicoesTarefas() <br>`); 
+//     document.querySelector(".console").insertAdjacentHTML("beforeend", `tarefas: ${JSON.stringify(tarefas)} <br>`); 
+    
+//     novasTarefas = [...tarefas];
+    
+//     document.querySelector(".console").insertAdjacentHTML("beforeend", `novas tarefas: ${JSON.stringify(novasTarefas)} <br>`); 
+
+//     novasTarefas.forEach((tarefa, index) => {
+
+//         document.querySelector(".console").insertAdjacentHTML("beforeend", `ENTROU NO FOREACH DE NOVAS TAREFAS <br>`); 
+        
+//         document.querySelector(".console").insertAdjacentHTML("beforeend", `ANTES - novas tarefas[${index}]: ${JSON.stringify(novasTarefas[index])} <br>`); 
+
+//         calculaHorarios2(index);
+//         tarefa.inicio = inicioTarefa; 
+//         tarefa.final = finalTarefa;
+//         document.querySelectorAll(".tarefa-item")[index].id = `tarefa-${index+1}`;
+//         let textoTarefa = `${tarefa.inicio} - ${tarefa.nome} (${duracaoString})`; 
+//         document.getElementById(`tarefa-${index+1}`).innerText = textoTarefa; 
+
+//         document.querySelector(".console").insertAdjacentHTML("beforeend", `DEPOIS - novas tarefas[${index}]: ${JSON.stringify(novasTarefas[index])} <br>`); 
+//     }); 
+    
+//     // for (let i = 0; i < document.querySelectorAll(".tarefa-item").length; i++) {
+//     //     document.querySelectorAll(".tarefa-item").id = `tarefa-${i}`;
+//     // }
+ 
+// }
